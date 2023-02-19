2c6842f857a4e98bf86e8e0a8259b1499013f29f
klalicki <32229646+klalicki@users.noreply.github.com>
2023-02-19 09:40:47 -0500

    added support for multiple googly eyes on a page

--- googly.js
+++ googly.js
@@ -1,18 +1,28 @@
 const eyeCoords = { x: 0, y: 0 };
 const updateEyeCoords = () => {
-  const tempRect = document.querySelector(".googly").getBoundingClientRect();
-  eyeCoords.x = (tempRect.left + tempRect.right) / 2;
-  eyeCoords.y = (tempRect.top + tempRect.bottom) / 2;
+  document.querySelectorAll(".googly").forEach((el) => {
+    const tempRect = el.getBoundingClientRect();
+    const eyeCoords = { x: 0, y: 0 };
+    eyeCoords.x = (tempRect.left + tempRect.right) / 2;
+    eyeCoords.y = (tempRect.top + tempRect.bottom) / 2;
+    el.setAttribute("data-x", eyeCoords.x);
+    el.setAttribute("data-y", eyeCoords.y);
+  });
+
   console.log(eyeCoords);
 };
 const checkOffset = ({ x, y }) => {
-  const delta = { x: x - eyeCoords.x, y: y - eyeCoords.y };
+  document.querySelectorAll(".googly").forEach((el) => {
+    const eyeCoords = {
+      x: el.getAttribute("data-x"),
+      y: el.getAttribute("data-y"),
+    };
+    const delta = { x: x - eyeCoords.x, y: y - eyeCoords.y };
 
-  const angle = (Math.atan2(delta.y, delta.x) * 180) / Math.PI;
-  console.log(angle);
-  document
-    .querySelector(".googly")
-    .style.setProperty("--googly-rotate", `${angle}deg`);
+    const angle = (Math.atan2(delta.y, delta.x) * 180) / Math.PI;
+    console.log(angle);
+    el.style.setProperty("--googly-rotate", `${angle}deg`);
+  });
 };
 
 updateEyeCoords();
